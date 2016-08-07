ae27a5f92ea63dae25dadc95c2041919af7c4d90
kbxu <404464154@qq.com>
2016-08-07 14:13:46 +0800

    changes

--- brant_utils/brant_headmotion_est/brant_HeadMotionEstimate.m
+++ brant_utils/brant_headmotion_est/brant_HeadMotionEstimate.m
@@ -27,21 +27,23 @@ for i = 1:SubjNum
     
     %%  refer van Dijk et al., Neuroimage 2012
     
-    for j = 1:size(head_motion, 1)
-        Motion(j) = norm(head_motion(j, 1:3)); %#ok<AGROW>
+    motion_diff = diff(head_motion);
+    
+    for j = 1:size(motion_diff, 1)
+        Motion(j) = norm(motion_diff(j, 1:3)); %#ok<AGROW>
     end
     HeadMotion.maxmotion_dijk(i) = max(Motion);
     HeadMotion.meanmotion_dijk(i) = mean(Motion);
     HeadMotion.numbermove_dijk(i) = sum(Motion > 0.1);
-    phi = head_motion(:, 4);
-    theta = head_motion(:, 5);
-    psi = head_motion(:, 6);
+    phi = motion_diff(:, 4);
+    theta = motion_diff(:, 5);
+    psi = motion_diff(:, 6);
     EularAngle = acosd((cos(phi) .* cos(theta) + cos(phi) .* cos(psi) + cos(theta) .* cos(psi) + sin(phi) .* sin(theta) .* sin(psi) - 1) / 2);
     HeadMotion.rotation_dijk(i) = mean(abs(EularAngle)); % in degree
     
     %%  refer Power et al., Neuroimage 2012
 
-    FD = sum([abs(head_motion(:, 1:3)), 50 * abs(head_motion(:, 4:6))], 2);
+    FD = sum([abs(motion_diff(:, 1:3)), 50 * abs(motion_diff(:, 4:6))], 2);
     HeadMotion.FD(i) = mean(FD);
     HeadMotion.num_badFD(i) = sum(FD >= 0.5);
 end

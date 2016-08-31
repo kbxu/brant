8c092b09c8c3ca310a2187b03e7455adfd2556da
kbxu <404464154@qq.com>
2016-09-01 00:19:47 +0800

    add support for multiple linux file browser

--- so.m
+++ so.m
@@ -3,6 +3,13 @@ function so(cmd)
 % cmd can either be a path or a command in matlab search path
 % kb on 20150417
 
+% seems a little buggy on linux mint18
+linux_f_browser = {'caja',...   %Mate
+                   'nemo',...   %Cinnamon
+                   'thunar',... %xfce
+                   'KDE',...    %dolphin
+                   'nautilus'}; %ubuntu and mac maybe?
+
 if exist(cmd, 'file') == 2
     pth = fileparts(which(cmd));
     if exist(pth, 'dir') ~= 7
@@ -16,7 +23,13 @@ switch(computer)
     case {'PCWIN64', 'PCWIN'}
         cmd_str = ['explorer', 32, pth];
     case {'GLNXA64', 'MACI64'}
-        cmd_str = ['nautilus', 32, pth];
+        chk_cmd = cellfun(@(x) system(['which', 32, x]), linux_f_browser);
+        cmd_ind = find(chk_cmd == 0, 1);
+        if ~isempty(cmd_ind)
+            cmd_str = [linux_f_browser{cmd_ind}, 32, pth];
+        else
+            error('Unknown file broswer!');
+        end
     otherwise
         error('oooooOperation system not recognized!ooooo');
 end
